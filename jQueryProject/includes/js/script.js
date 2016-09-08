9f09ea3bd6db61ab6e276b87fb2aedc99a35d8d6
daminiverma786 <daminiverma786@gmail.com>
2016-09-08 18:36:31 +0530

    Jquer Project

---
+++ jQueryProject/includes/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function()
+{
+	var $tb = $('#tbd');
+	
+	$('#search').on('click',function(){
+
+		var $finddata = $('#movietitle').val();
+		
+		$tb.empty();
+		$.ajax({
+			url:'http://www.omdbapi.com/?s='+$finddata,
+			type:'GET',
+			
+			datatype:'json',
+
+			success:function(data){
+
+
+				 $.each(data.Search,function(i,movie)
+				{
+
+
+					$tb.append('<tr>'+'<td>'+"<img src="+movie.Poster+' alt="Image not visible" class="img-responsive">"'+'</td>'+'<td>'+movie.Title+'</td>'+'<td>'+movie.Type+'</td>'+'<td>'+movie.Year+'</td>'+'<td>'+movie.imdbID+'</td>'+'</tr>');
+
+				})
+		
+				}
+
+
+
+			});
+	});
+});
