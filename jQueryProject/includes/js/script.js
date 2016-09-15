37d557df9ddc3e9e21ec421dc696815d8f124cbb
daminiverma786 <>
2016-09-15 18:06:52 +0530

    Final Commit

--- jQueryProject/includes/js/script.js
+++ jQueryProject/includes/js/script.js
@@ -22,8 +22,12 @@ $(document).ready(function()
 
 					$tb.append('<tr>'+'<td>'+"<img src="+movie.Poster+' alt="Image not visible" class="img-responsive">"'+'</td>'+'<td>'+movie.Title+'</td>'+'<td>'+movie.Type+'</td>'+'<td>'+movie.Year+'</td>'+'<td>'+movie.imdbID+'</td>'+'</tr>');
 
-				})
+				});
 		
+				},
+				error: function()
+				{
+					alert('No movies found');
 				}
 
 
